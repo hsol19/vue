d9c76c20e4bb4946d22ef301f2149cbbbdfceec7
hsol19 <kimhgst919@naver.com>
2022-07-25 16:39:29 +0900

    vue0725

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <!-- (item,index in 배열의 이름) -->
+    <figure v-for="(item,index) in products" :key="index">
+      <img :src="item.image" :alt="item.name" />
+      <figcaption>
+        <dl>
+          <dt>{{ite.title}}</dt>
+          <dd>{{item.name}}</dd>
+          <p>{{item.price}}</p>
+        </dl>
+      </figcaption>
+    </figure>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SubTwoData from '@/assets/data.json'
+// import SubTwo from '@/components/SubTwo.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return {
+     products:SubTwoData 
+    }
+  } 
+  // ,
+  // created(){
+  //   fetch('./assets/data.json')
+  //   .then(response => response.json()
+  //   .then(data =>this.products = data))
+  // }
+  ,
+  components:{
+    // SubTwo 
+  },
+  computed:{},
+  methods:{}
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+<style scoped>
+img{
+  max-width: 100%;
+}
+
+div{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
 }
 </style>
---
+++ src/components/SubTwo.vue
@@ -0,0 +1,52 @@
+<template>
+    <figure>
+      <img :src="item.image" :alt="item.name" />
+      <figcaption>
+        <dl>
+          <dt></dt>
+          <dd>{{item.name}}</dd>
+          <p>{{item.price}}</p>
+        </dl>
+      </figcaption>
+    </figure>
+</template>
+
+
+<script>
+export default {
+  props:{
+        item:{type:Object}
+    }
+}
+</script>
+
+<style scoped>
+/* #box02>#section{
+  width: 95.83333333333333%;
+  margin: 0 auto;
+}
+#box02>section{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around
+} */
+
+figure{
+  width:22%;
+  margin-bottom: 60px;
+}
+figure>img{
+  margin-bottom: 20px;
+}
+figure>figcaption>dl>dt{
+  margin-bottom: 20px;
+  font-size: 15px;
+  font-weight: bold;
+}
+figure>figcaption>dl>dd{
+  margin-bottom: 20px;
+}
+figure>figcaption>dl>p{
+  color: red;
+}
+</style>
